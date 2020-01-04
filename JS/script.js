7213567b2faa55712f82e050776eecc32882cb45
Artem <trofimartya@gmail.com>
2020-01-04 13:09:57 +0200

    jsVer3

--- JS/script.js
+++ JS/script.js
@@ -1,2 +1,5 @@
-var alrt = alert ('Hello to me!');
-console.log(alrt);
+var age=prompt("Enter age");
+var gender = prompt("Enter gender");
+if (age>18 && gender==="female"){
+    alert ("Youre woman!")
+}else { alert ("Youre girl!")};
